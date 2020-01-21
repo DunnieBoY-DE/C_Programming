2511dd856edf354704cba2f339b9242d3c4dee25
Dunnieboy <duncanlinnaeus98@gmail.com>
2020-01-21 17:54:46 +0000

    Working with data_types

---
+++ data_types.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //working with data types
+    int age = 40;
+    double gpa = 3.6;
+    char grade = 'A';
+    char phrase[] = "Dee Xperts";
+
+    return 0;
+}
